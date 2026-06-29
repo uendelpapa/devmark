e34ffd3384188b549b58bd05f715966d2cc5d553
uendelpapa <uendelpapa@gmail.com>
2026-06-29 20:53:05 -0300

    conf deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [
+    devtools(),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
 })
 
 export default config
